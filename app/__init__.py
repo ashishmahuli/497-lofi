94a5349b91a704b8bfb568f7d1ecfe7bf72d8f1a
Ashish Mahuli <ashish.mahuli@gmail.com>
2021-11-03 11:19:27 -0400

    bootstrap + functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, session
+from fillpdf import fillpdfs
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.config['SECRET_KEY'] = 'my-secret-key'
+
+def generate_questions():
+    pdf_template = "app/static/test-copy.pdf"
+    x= fillpdfs.get_form_fields(pdf_template)
+    return list(x.keys())
+
+questions = generate_questions()
+
+
 
 from app import app_routes
--- app/app_routes.py
+++ app/app_routes.py
@@ -1,60 +1,63 @@
-from app import app
-from flask import render_template, flash, redirect, session
+from app import app, questions
+from flask import render_template, flash, redirect, session, send_file, send_from_directory
 from app.form import LoginForm
 from wtforms.fields.core import Label
 from fillpdf import fillpdfs
 
+# Helper functions for managing inputed field data in session object
+def get_fields():
+    if "fields" in session:
+        return session["fields"]
+    else:
+        return {}
+
+def set_field(field_num, val):
+    if "fields" not in session:
+        session["fields"] = {}
+        
+    session["fields"][field_num] = val
+    session.modified = True
+
+
+# Main Route
 @app.route('/')
 def home():
-
     print(session)
     return render_template('main.html', title="Home")
 
-questions = [
-    "Alien Registration Number",
-    "U.S Social Security Number",
-    "USCIS Online Account Number",
-    "Complete Last Name",
-    "First Name",
-    "Middle Name"
-]
 
+# Route for asking user to fill a field
 @app.route('/test-form/<field_num>', methods=['GET', 'POST'])
-def login(field_num):
+def ask_question(field_num):
     
     form = LoginForm()
-    form.field1.label = Label("post", questions[int(field_num)])
+    question = questions[int(field_num)]
+    form.field1.label = Label("post", question)
     if form.validate_on_submit():
-        
 
-        #if field_num != '0':
-        #    print(session["fields"][str(int(field_num) - 1)])
-        if "fields" not in session:
-            session["fields"] = {}
-
-        session["fields"][field_num] = form.field1.data
-        session.modified = True
-        
-
+        set_field(question, form.field1.data)
         return redirect('/test-form/' + str(int(field_num) + 1))
-    return render_template('form.html', title='Sign In', form=form)
+
+    return render_template('form.html', field_num = field_num, form=form)
+
+# Route for showing user the fields they have filled
+@app.route('/confirm')
+def confirm():
+    return render_template('confirm.html', title="Home", dict=get_fields())
+
+# Route for sending completed form
+@app.route('/get-form')
+def get_filled_form():
+    pdf_template = "app/static/test-copy.pdf"
+    pdf_output = "app/static/output.pdf"
+    
+    fillpdfs.write_fillable_pdf(pdf_template, pdf_output, get_fields())
+    return send_from_directory(app.static_folder, "output.pdf")
 
 
-@app.route('/get_filled')
-def get_filled():
-    pdf_template = "i-589_form.pdf"
-    pdf_output = "output.pdf"
-    print(fillpdfs.get_form_fields(pdf_template))
-    '''data_dict = {
-        'Text2': 'Name',
-        'Text4': 'LastName',
-        'box': 'Yes',
-    }
 
-    fillpdfs.write_fillable_pdf(pdf_template, 'new.pdf', data_dict)
 
-    # If you want it flattened:
-    fillpdfs.flatten_pdf('new.pdf', 'newflat.pdf')'''
+   
 
 
     
